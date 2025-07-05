18e5c54e90861d69a8d92a0fd53c1a68dfc9a08b
ShristiSingh065 <mahak.0655@gmail.com>
2025-07-05 17:13:08 +0530

    Add files via upload

--- recipe_model.py
+++ recipe_model.py
@@ -8,7 +8,7 @@ def load_classification_model():
     model = AutoModelForImageClassification.from_pretrained("Shresthadev403/food-image-classification")
     return processor, model
 def load_text_generator():
-    return pipeline( "text2text-generation",model="openai/clip-vit-base-patch32")
+    return pipeline( "text2text-generation",model="google/flan-t5-small")
 processor, model = load_classification_model()
 text_generator = load_text_generator()
 
